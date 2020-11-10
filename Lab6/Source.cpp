96e492ac896b251ba24e6d0d8c45eb3a27ba7e5d
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-11-09 19:57:27 -0600

    All error checking place for reading an converting binary from input file to decimal

--- Lab6/Source.cpp
+++ Lab6/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 
+
 using namespace std;
 
 int main()
@@ -8,15 +9,18 @@ int main()
 	ifstream inFile;
 	char input;
 
+	cout << "Binary Number:" << "\t\tDecimal Equivalent:" << endl;
+
 	inFile.open("test.txt");
 
-	inFile.get(input); //Priming read of input file
+	inFile.get(input);
 
 	int decimalNum = 0;
 	int position = 0;
 	string binaryNum = "";
 	while (inFile) {
 
+		
 		if (input == '1') {
 
 			binaryNum += input;
@@ -30,11 +34,10 @@ int main()
 			position++;
 		}
 		else if (input == '\n' && position == 0) { //If the cursor reads a newline character on the first line for a character
-
-			inFile.get(input); //Get the input character on the next line
-			continue; //Re-evaluate logic from beginnig of the loop with the new character.
+			inFile.get(input);
+			continue; //Re-evaluate logic from beginning of the loop with the new character.
 		}
-		else if ((int(input) < '0' || int(input) > '1') && (input != ' ')) { //If the ASCII code is less than '0' or greater than '1' AND NOT space (32 ASCII code)
+		else if ((int(input) < '0' || int(input) > '1') && (input != ' ') || (input == ' ' && position > 0)) { //If the ASCII code is less than '0' or greater than '1' AND NOT space (32 ASCII code)
 
 			cout << "Bad data on input\n"; //Print error message to the console
 
@@ -49,7 +52,7 @@ int main()
 
 		inFile.get(input);
 
-		if ((input == '\n' && position > 0) || (!inFile)) { //If file input cursor has read a new line or reached end of file
+		if ((input == '\n' && position > 0) || (!inFile)) { //If file input cursor has read a new line where the position is atleast one OR reached end of file
 
 			cout << binaryNum << ' ' << decimalNum << endl; //Display the current binary number and decimal number computed for the line
 
@@ -58,6 +61,7 @@ int main()
 			decimalNum = 0;
 			binaryNum = "";
 		}
+		
 	}
 
 
