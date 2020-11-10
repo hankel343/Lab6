bcceb585104a6b74a49380886ea6cceb3eb82197
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-11-09 19:09:57 -0600

    Input validation implemented Only missing logic to handle spaces between input.

--- Lab6/Source.cpp
+++ Lab6/Source.cpp
@@ -1,66 +1,67 @@
 #include <iostream>
 #include <fstream>
 
-
 using namespace std;
-
-void ReadFile(ifstream& inFile);
 
 int main()
 {
-    ifstream inFile;
+	ifstream inFile;
+	char input;
+
+	inFile.open("test.txt");
+
+	inFile.get(input); //Priming read of input file
+
+	int decimalNum = 0;
+	int position = 0;
+	string binaryNum = "";
+	while (inFile) {
+
+		if (input == '1') {
+
+			binaryNum += input;
+			decimalNum = (decimalNum * 2) + 1;
+			position++;
+		}
+		else if (input == '0') {
+
+			binaryNum += input;
+			decimalNum = (decimalNum * 2) + 0;
+			position++;
+		}
+		else if (input == '\n' && position == 0) { //If the cursor reads a newline character on the first line for a character
+
+			inFile.get(input); //Get the input character on the next line
+			continue; //Re-evaluate logic from beginnig of the loop with the new character.
+		}
+		else if ((int(input) < '0' || int(input) > '1') && (input != ' ')) { //If the ASCII code is less than '0' or greater than '1' AND NOT space (32 ASCII code)
+
+			cout << "Bad data on input\n"; //Print error message to the console
+
+			//Resets counters for the next line of input
+			position = 0;
+			decimalNum = 0;
+			binaryNum = "";
+
+			//Ignores all data on the input line with bad data
+			inFile.ignore(1000, '\n');
+		}
+
+		inFile.get(input);
+
+		if ((input == '\n' && position > 0) || (!inFile)) { //If file input cursor has read a new line or reached end of file
+
+			cout << binaryNum << ' ' << decimalNum << endl; //Display the current binary number and decimal number computed for the line
+
+			//Resets counters for the next line of input
+			position = 0;
+			decimalNum = 0;
+			binaryNum = "";
+		}
+	}
 
 
-    ReadFile(inFile);
 
 
-    return 0;
-}
-
-void ReadFile(ifstream& inFile) {
-
-    char input;
-    int total = 0, num = 0;
-    string binaryNum = "";
-
-    inFile.open("test.txt");
-
-    if (!inFile) { //If input file fails to open
-        cout << "The input file failed to open\n";
-    }
-
-
-
-    while (inFile.get(input)) {
-
-        if (input == '1') { //Checks to see if input is a 1
-            binaryNum += input; //Adds the '1' to the binaryNum string
-            total = (total * 2) + 1; // Multiplying by two accounts for the next highest place value in binary
-            num++; //Reads place value in binary
-        }
-        else if (input == '0') { //Checks to see if input is a 0
-            binaryNum += input; //Adds a '0' to the binaryNum string
-            total = (total * 2) + 0; //Multiplying by two accounts for the next hightest place value in binary, and the current value isn't added because it is a 0.
-            num++;
-        }
-        else if ((isalpha(input)) || (input == ' ' && num > 0)) { //if (input(is a letter)) OR there is a space between binary values)
-            binaryNum = "";
-            total = 0;
-            num = 0;
-            cout << "Bad data on input" << endl;
-            inFile.ignore(1000, '\n');
-        }
-        else if (input == '\n' && num == 0) {
-            continue;
-        }
-
-        if (input == '\n' && num > 0) {
-            cout << binaryNum << ' ' << total << endl;
-            total = 0;
-            num = 0;
-            binaryNum = "";
-        }
-
-    }
-
+	return 0;
 }
